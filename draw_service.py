30ea5f1c15f8bd958c8c6026a7b086636968475e
loasan <lien@oppo.com>
2022-03-23 12:32:52 +0800

    delete demo data

--- draw_service.py
+++ draw_service.py
@@ -9,10 +9,8 @@ app = Flask(__name__)
 DATABASE = "database.db"
 TABLE_NAME = "name_dict"
 
-NAME_LIST = ["Lily", "John", "Lucy", "chris", "james", "young", "shirley", "crampton",
-             "vincent", "fred", "andrea", "alex", "marie", "owen", "lewis", "bobby", "kent", "jeffery"]
-ID_LIST = ["1", "2", "3", "4", "5", "6", "7", "8", "9",
-           "10", "11", "12", "13", "14", "15", "16", "17", "18"]
+NAME_LIST = []
+ID_LIST = []
 
 INSERT_OR_REPLACE_SQL = "INSERT OR REPLACE INTO "+TABLE_NAME+" VALUES (?,?,?)"
 SELECT_SQL = "SELECT * FROM " + TABLE_NAME
